a8220aebe29691528e263afbb44ddae3699e4366
qakqs <1248001568@qq.com>
2023-05-27 00:57:15 +0800

    完成初步开发

--- src/main/java/cn/pacman/db/router/dynamic/DynamicMybatisPlugin.java
+++ src/main/java/cn/pacman/db/router/dynamic/DynamicMybatisPlugin.java
@@ -1,20 +1,59 @@
 package cn.pacman.db.router.dynamic;
 
+import cn.pacman.db.router.DBContextHolder;
+import cn.pacman.db.router.annoation.DBRouterStrategy;
 import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Intercepts({@Signature(type = StatementHandler.class, method = "prepare",
         args = {Connection.class, Integer.class})})
 public class DynamicMybatisPlugin implements Interceptor {
+    private Pattern pattern = Pattern.compile("(from|into|update)[\\s]{1,}(\\w{1,})", Pattern.CASE_INSENSITIVE);
+
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        return null;
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        MetaObject metaObject = MetaObject.forObject(statementHandler, SystemMetaObject.DEFAULT_OBJECT_FACTORY,
+                SystemMetaObject.DEFAULT_OBJECT_WRAPPER_FACTORY, new DefaultReflectorFactory());
+        MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+        String id = mappedStatement.getId();
+        String className = id.substring(0, id.lastIndexOf("."));
+        Class<?> clazz = Class.forName(className);
+        DBRouterStrategy dbRouterStrategy = clazz.getAnnotation(DBRouterStrategy.class);
+        if (null == dbRouterStrategy || !dbRouterStrategy.splitTable()) {
+            return invocation.proceed();
+        }
+        // 获取SQL
+        BoundSql boundSql = statementHandler.getBoundSql();
+        String sql = boundSql.getSql();
+
+        Matcher matcher = pattern.matcher(sql);
+        String tableName = null;
+        if (matcher.find()) {
+            tableName = matcher.group().trim();
+        }
+        assert null != tableName;
+        String replaceSql = matcher.replaceAll(tableName + "_" + DBContextHolder.getTBKey());
+        // 通过反射修改SQL语句
+        Field field = boundSql.getClass().getDeclaredField("sql");
+        field.setAccessible(true);
+        field.set(boundSql, replaceSql);
+        field.setAccessible(false);
+        return invocation.proceed();
     }
 
     @Override
@@ -25,5 +64,18 @@ public class DynamicMybatisPlugin implements Interceptor {
     @Override
     public void setProperties(Properties properties) {
         Interceptor.super.setProperties(properties);
+    }
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("(from|into|update)[\\s]{1,}(\\w{1,})", Pattern.CASE_INSENSITIVE);
+        String sql = " select * from  ttt   where  tentid = A001  ";
+        Matcher matcher = pattern.matcher(sql);
+        String tableName = null;
+        if (matcher.find()) {
+            tableName = matcher.group().trim();
+        }
+        System.out.println(tableName);
+        String replaceSql = matcher.replaceAll(tableName + "_" + "key");
+        System.out.println(replaceSql);
     }
 }
